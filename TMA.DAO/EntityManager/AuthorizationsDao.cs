922294dbc18ae9ce119e106b5ac87ec845033c2a
gaea <gaea.uv@gmail.com>
2013-04-10 11:44:00 -0500

    modelo autorization

---
+++ TMA.DAO/EntityManager/AuthorizationsDao.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using NHibernate.Criterion;
+using TMA.MODEL.Entity;
+
+namespace TMA.DAO.EntityManager
+{
+    public class AuthorizationsDao : Dao
+    {
+        public static List<Authorization> findBy(string field, int value)
+        {
+            List<Authorization> authorizations = (List<Authorization>)Session.CreateCriteria<Authorization>()
+                .Add(Restrictions.Eq(field, value))
+                .List<Authorization>();
+
+            return authorizations;
+        }
+
+        public static Authorization findBy(string field, string value)
+        {
+            Authorization authorization = (Authorization)Session.CreateCriteria<Authorization>()
+                .Add(Restrictions.Eq(field, value))
+                .UniqueResult();
+
+            return authorization;
+        }
+
+        public static Authorization find(int Id_Authorization)
+        {
+            Authorization authorization = (Authorization)Session.CreateCriteria<Authorization>()
+                .Add(Restrictions.Eq("Id_Authorization", Id_Authorization))
+                .UniqueResult();
+
+            return authorization;
+        }
+
+        public static List<Authorization> findAll()
+        {
+            IList<Authorization> iAuthorizations = Session.CreateCriteria<Authorization>().List<Authorization>();
+
+            return (iAuthorizations != null) ? new List<Authorization>(iAuthorizations) : new List<Authorization>();
+        }
+
+        public static void save(Authorization authorization)
+        {
+            using (ITransaction transaction = Session.BeginTransaction())
+            {
+                try
+                {
+                    Session.Save(authorization);
+
+                    transaction.Commit();
+                    Session.Flush();
+                }
+                catch (Exception exception)
+                {
+                    transaction.Rollback();
+                    throw exception;
+                }
+            }
+        }
+
+        public static void update(Authorization authorization)
+        {
+            using (ITransaction transaction = Session.BeginTransaction())
+            {
+                try
+                {
+                    Session.Clear();
+                    Session.Update(authorization);
+
+                    transaction.Commit();
+                    Session.Flush();
+                }
+                catch (Exception exception)
+                {
+                    transaction.Rollback();
+                    throw exception;
+                }
+            }
+        }
+
+        public static void delete(Authorization authorization)
+        {
+            using (ITransaction transaction = Session.BeginTransaction())
+            {
+                try
+                {
+                    Session.Delete(authorization);
+
+                    transaction.Commit();
+                    Session.Flush();
+                }
+                catch (Exception exception)
+                {
+                    transaction.Rollback();
+                    throw exception;
+                }
+            }
+        }
+    }
+}
---
+++ TMA.MODEL/Entity/Authorization.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMA.MODEL.Entity
+{
+    [Serializable]
+    public class Authorization
+    {
+        #region Attributes
+
+        private int id_Authorization;
+        private string identification;
+        private bool authorized;
+        private DateTime? startDate;
+        private DateTime? endDate;
+        private int? id_PersonAuthorizing;
+        private int? id_AuthorizationType;
+        private string reason;
+        private int id_Person;
+
+        #endregion Attributes
+
+        #region Properties
+
+        public virtual int Id_Person
+        {
+            get { return id_Person; }
+            set { id_Person = value; }
+        }
+
+        public virtual string Reason
+        {
+            get { return reason; }
+            set { reason = value; }
+        }
+
+        public virtual int? Id_AuthorizationType
+        {
+            get { return id_AuthorizationType; }
+            set { id_AuthorizationType = value; }
+        }
+
+        public virtual int? Id_PersonAuthorizing
+        {
+            get { return id_PersonAuthorizing; }
+            set { id_PersonAuthorizing = value; }
+        }
+
+        public virtual DateTime? EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        public virtual DateTime? StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        public virtual bool Authorized
+        {
+            get { return authorized; }
+            set { authorized = value; }
+        }
+
+        public virtual int Id_Authorization
+        {
+            get { return id_Authorization; }
+            set { id_Authorization = value; }
+        }
+
+        public virtual string Identification
+        {
+            get { return identification; }
+            set { identification = value; }
+        }
+
+        #endregion Properties
+    }
+}
