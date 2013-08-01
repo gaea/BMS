e19159f4e611c20bd61b1092cde27ce286c4c8a6
gaea <gaea.uv@gmail.com>
2013-08-01 00:46:08 -0500

    se adiciono modulo de visualizacion de auditoria

---
+++ TMA.DAO/EntityManager/AuditEntryDao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using NHibernate.Criterion;
+using TMA.MODEL.Entity;
+
+namespace TMA.DAO.EntityManager
+{
+    public class AuditEntryDao : Dao
+    {
+        public static int Count()
+        {
+            Session.Clear();
+
+            IList<AuditEntry> iAuditEntry = Session.CreateCriteria<AuditEntry>().List<AuditEntry>();
+
+            return (iAuditEntry != null) ? iAuditEntry.Count : 0;
+        }
+
+        public static List<AuditEntry> findAll(int start, int limit)
+        {
+            Session.Clear();
+
+            IList<AuditEntry> iAuditEntry = Session.CreateCriteria<AuditEntry>()
+                .AddOrder(Order.Desc("Id"))
+                .SetFirstResult(start)
+                .SetMaxResults(limit)
+                .List<AuditEntry>();
+
+            return (iAuditEntry != null) ? new List<AuditEntry>(iAuditEntry) : new List<AuditEntry>();
+        }
+    }
+}
---
+++ TMA.MODEL/Entity/AuditEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMA.MODEL.Entity
+{
+    [Serializable]
+    public class AuditEntry
+    {
+        public virtual int Id { get; set; }
+        public virtual int EntryPerson { get; set; }
+        public virtual DateTime RecordDate { get; set; }
+        public virtual string Reason { get; set; }
+    }
+}
